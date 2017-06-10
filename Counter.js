3f2b0d687a332be4b770a6da85b3c2912eadc1b8
kmsheng <kmsh3ng@gmail.com>
2017-06-11 00:25:53 +0800

    add argument onIncrementAsync function

--- Counter.js
+++ Counter.js
@@ -1,6 +1,6 @@
 import React, {Component, PropTypes} from 'react'
 
-const Counter = ({ value, onIncrement, onDecrement }) =>
+const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) =>
       <div>
         <button onClick={onIncrement}>
           Increment
