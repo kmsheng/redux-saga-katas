a666b1a58f4e73a5aa768fb2ffd27d2678b9044b
kmsheng <kmsh3ng@gmail.com>
2017-06-11 01:03:14 +0800

    add login reducer details

--- reducers.js
+++ reducers.js
@@ -1,12 +1,8 @@
-export default function counter(state = 0, action) {
+export default function login(state = {user: null}, action) {
   switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'INCREMENT_IF_ODD':
-      return (state % 2 !== 0) ? state + 1 : state
-    case 'DECREMENT':
-      return state - 1
+    case 'LOGIN_SUCCESS':
+      return {user: action.data};
     default:
-      return state
+      return state;
   }
 }
